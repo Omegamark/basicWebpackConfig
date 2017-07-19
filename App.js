3139a065bc0adc00b4a47c9e1aecc87b1abbf625
Omegamark <grant.mark@gmail.com>
2017-07-19 13:30:12 -0600

    completed react & webpack hello world from scratch

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Fucking work React!!!!... well webpack!!!!!
+      </div>
+    );
+  }
+}
+
+export default App;
